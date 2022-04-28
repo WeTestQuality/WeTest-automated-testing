7e177d37cb9496a308bfcbe3f0503505ce60b6be
calvinguo <2443326+calvinguo@users.noreply.github.com>
2022-04-28 23:17:28 +0800

    Update appium_local_android.py
     refactor: print session id

--- samples/Android/Appium_local/appium_local_android.py
+++ samples/Android/Appium_local/appium_local_android.py
@@ -13,27 +13,30 @@ class WeTestAppiumTest(unittest.TestCase):
         desired_caps = {
 
             # Appium native capabilities
-            'platformName': 'Android', # Which mobile OS platform to use
-            'deviceName': 'Android Phone',# The kind of mobile device or emulator to use
-            'automationName': 'UiAutomator2',# Which automation engine to use
-            'newCommandTimeout': 60, # How long (in seconds) Appium will wait for a new command from the client before assuming the client quit and ending the session
-            
+            'platformName': 'Android',  # Which mobile OS platform to use
+            'deviceName': 'Android Phone',  # The kind of mobile device or emulator to use
+            'automationName': 'UiAutomator2',  # Which automation engine to use
+            'newCommandTimeout': 60,
+            # How long (in seconds) Appium will wait for a new command from the client before assuming the client quit and ending the session
+
             # WeTest capabilities
-            'wetest_secret_id': '', #This is available in the Accounts view
-            'wetest_secret_key': '', #This is available in the Accounts view
-            'wetest_app_id': '', #  Specifies the Application file (.app/.apk) to be installed on the device. 
-            'wetest_device_id': 0, # WeTest device id
-            'wetest_cloud_id': 0, # WeTest device cloud id
-            'wetest_project_id': "", # WeTest project id
-            'wetest_test_timeout': 360, # The timeout for the whole test execution (in seconds)   
+            'wetest_secret_id': '',  # This is available in the Accounts view
+            'wetest_secret_key': '',  # This is available in the Accounts view
+            'wetest_app_id': '',  # Specifies the Application file (.app/.apk) to be installed on the device.
+            'wetest_device_id': 0,  # WeTest device id
+            'wetest_cloud_id': 0,  # WeTest device cloud id
+            'wetest_project_id': '',  # WeTest project id
+            'wetest_test_timeout': 360,  # The timeout for the whole test execution (in seconds)
         }
-        
+
         # WeTest Appium WebDriver address
-        # When running Appium locally, the web driver address is running on a localhost (http://localhost:4723/wd/hub). 
+        # When running Appium locally, the web driver address is running on a localhost (http://localhost:4723/wd/hub).
         # When running the test from your local machine against a WeTest cloud device, you need to change the Appium server location.
         print("WeTest WebDriver request initiated. Waiting for response, this typically takes a few mins")
-        self.driver = webdriver.Remote("https://service-ifvrcbba-1251501889.gz.apigw.tencentcs.com/wd/hub", desired_caps)
-        print("Connecting to WeTest WebDriver successfully")
+        self.driver = webdriver.Remote("https://service-ifvrcbba-1251501889.gz.apigw.tencentcs.com/wd/hub",
+                                       desired_caps)
+
+        print("Connecting to WeTest WebDriver successfully with Session ID:", self.driver.session_id)
 
     def tearDown(self):
         print('Quiting')
@@ -51,7 +54,7 @@ class WeTestAppiumTest(unittest.TestCase):
         time.sleep(5)
         print('test case 2 end.')
     # your test cases end here
-    
+
 
 if __name__ == '__main__':
     unittest.main()
